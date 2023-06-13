9ac3dd29332e4c5b5a7c91e922b1e2777b7b9316
ress <haruki20050202@icloud.com>
2023-06-13 23:21:45 +0900

    完了未完了の実装が完了

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 $("#addTodo").click(function() {
     const inputTodo = $("input").val();
       $("#todoiLst").append("<li><input type='checkbox'>" + inputTodo + "</li>");
-      
+
       $("input").val("");
     }
 );
+
+$(document).on("chenge", "input[type=checkbox]", function(){
+
+
+    if($(this).is(":checked")) {
+        $(this).parent().css("text-decoration", "line-through"); 
+    }else{
+        $(this).parent().css("text-decoration", "none"); 
+    }
+})
